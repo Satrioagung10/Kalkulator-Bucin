91e4511597d6134e47b2e90c39e586e7d9322246
Satrioagung10 <202210715114@mhs.ubharajaya.ac.id>
2023-09-21 22:27:29 +0700

    update

--- script.js
+++ script.js
@@ -1,21 +1,28 @@
 function calculate() {
-    var name1 = document.getElementById("name1").value;
-    var name2 = document.getElementById("name2").value;
-    var loveScore = Math.random() * 100;
-    loveScore = Math.floor(loveScore);
+  var name1 = document.getElementById("name1").value;
+  var name2 = document.getElementById("name2").value;
+  var hasil = document.getElementById("result");
+  var angka = document.getElementById("results");
+  var loveScore = Math.random() * 100;
+  loveScore = Math.floor(loveScore);
+  if (name1 || name2 != "") {
+    angka.innerHTML = loveScore + "%";
+
     if (loveScore >= 90) {
-      document.getElementById("result").innerHTML = " Kamu " + name1 + " dan pasangan mu " + name2 + " couple goals ";
+      hasil.innerHTML =
+        " Kamu " + name1 + " dan pasangan mu " + name2 + " couple goals ";
     } else if (loveScore >= 70) {
-      document.getElementById("result").innerHTML = " Kamu " + name1 + " dan pasangan mu " + name2 + " cocok banget ";
+      hasil.innerHTML =
+        " Kamu " + name1 + " dan pasangan mu " + name2 + " cocok banget ";
     } else if (loveScore >= 50) {
-      document.getElementById("result").innerHTML = " Kamu " + name1 + " dan pasangan mu " + name2 + " kurang PDKT ";
+      hasil.innerHTML =
+        " Kamu " + name1 + " dan pasangan mu " + name2 + " kurang PDKT ";
     } else {
-      document.getElementById("result").innerHTML = " Kamu " + name1 + " dan pasangan mu " + name2 + " masih asing ";
+      hasil.innerHTML =
+        " Kamu " + name1 + " dan pasangan mu " + name2 + " masih asing ";
     }
-
-    newFunction();
-
-    function newFunction() {
-        document.getElementById("results").innerHTML = loveScore + "%";
-    }
+  } else {
+    hasil.innerHTML = "Masukin nama nya dulu lah cuk";
+    angka.innerHTML = "";
   }
+}
